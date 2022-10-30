416c428bbe9d51678b432306419b71586c5bc3d2
MaheshG10 <92197783+MaheshG10@users.noreply.github.com>
2022-10-30 12:00:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
+import "./styles.css";
+const App = () => {
+  const [Users, setUsers] = useState([]);
+  const loadUsers = async () => {
+    console.log("before");
+    const response = await fetch("https://api.github.com/users");
+    const jsonResponse = await response.json();
+    setUsers(jsonResponse);
+  };
+  return (
+    <div className="App">
+      <h1>Hello All</h1>
+      <button onClick={loadUsers}>Get Data</button>
+      <h2>Users:</h2>
+      <ul>
+        {Users.map(({ id, login, avatar_url }) => (
+          <li key={id}>
+            Name: {login}
+            Avatar: {avatar_url}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
